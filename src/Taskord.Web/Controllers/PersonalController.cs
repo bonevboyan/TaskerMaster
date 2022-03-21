bcc8a8cd1f5d551cf9fa40ce413f112aaf76d19e
bonevboyan <boyanbonev43@gmail.com>
2022-03-21 10:34:14 +0200

    Create PersonalController.cs

---
+++ src/Taskord.Web/Controllers/PersonalController.cs
@@ -0,0 +1,39 @@
+﻿namespace Taskord.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Taskord.Data.Models;
+    using Taskord.Services.Users;
+    using Taskord.Web.Models;
+
+    public class PersonalController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public PersonalController(IUserService userService, UserManager<ApplicationUser> userManager)
+        {
+            this.userService = userService;
+            this.userManager = userManager;
+        }
+
+        [Authorize]
+        public IActionResult All()
+        {
+            var userId = this.userManager.GetUserId(this.User);
+
+            var friends = this.userService.GetUserFriendsList(userId);
+
+            return View(friends);
+        }
+
+        [Authorize]
+        public IActionResult Search([FromQuery] UserQueryModel query)
+        {
+            // TODO
+
+            return View();
+        }
+    }
+}
